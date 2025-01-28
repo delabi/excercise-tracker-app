5c12745332bd27fc8045a7a07c6decfada9c357d
Laban Bwire <bwire.laban@gmail.com>
2025-01-28 17:57:53 +0300

    Added dropdown menu to index.html

--- index.js
+++ index.js
@@ -119,6 +119,6 @@ app.get('/api/users/:_id/logs', async (req, res) => {
   }
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 3002, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 })
